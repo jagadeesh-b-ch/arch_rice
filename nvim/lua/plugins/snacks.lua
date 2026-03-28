af6176af31c0e4b7226eb9494b0e727a58433777
Jagadeesh Babu Chitta <jagadeesh7.chitta@gmail.com>
2026-03-28 12:21:37 +0530

    Updated gitignore to track all required files properly

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {
+      hidden = true, -- Show hidden files (dotfiles)
+      ignored = true, -- Show git-ignored files
+    },
+  },
+}
